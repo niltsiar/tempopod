8e36f0f92d60a76172ff074a58a790ea7dd40915
Borja Quevedo <borjota@gmail.com>
2024-03-21 18:30:08 +0100

    Rewrite parseEpisodes using functions from Kotlin standard library

--- app/src/jvmMain/kotlin/dev/niltsiar/tempopod/main.kt
+++ app/src/jvmMain/kotlin/dev/niltsiar/tempopod/main.kt
@@ -9,6 +9,7 @@ import java.util.function.Consumer
 import javax.xml.parsers.DocumentBuilderFactory
 import org.w3c.dom.Element
 import org.w3c.dom.Node
+import org.w3c.dom.NodeList
 import org.xml.sax.InputSource
 
 fun main(args: Array<String>) {
@@ -32,23 +33,25 @@ private fun fetchFeed(feedUrl: String): String {
 }
 
 private fun parseEpisodes(xmlContent: String): List<Episode> {
-    val episodes: MutableList<Episode> = ArrayList()
-    val factory = DocumentBuilderFactory.newInstance()
-    val builder = factory.newDocumentBuilder()
-    val document = builder.parse(InputSource(StringReader(xmlContent)))
-    val items = document.getElementsByTagName("item")
-    for (i in 0 until items.length) {
-        val item = items.item(i)
-        if (item.nodeType == Node.ELEMENT_NODE) {
-            val element = item as Element
+    val document = DocumentBuilderFactory.newInstance()
+        .newDocumentBuilder()
+        .parse(InputSource(StringReader(xmlContent)))
+
+    return document.getElementsByTagName("item")
+        .asSequence() // Convert NodeList to Sequence for more idiomatic Kotlin
+        .filterIsInstance<Element>() // Filter out non-Element nodes
+        .map { element ->
             val title = element.getElementsByTagName("title").item(0).textContent
             val durationStr = element.getElementsByTagName("itunes:duration").item(0).textContent
             val duration = durationStr.toInt()
-            episodes.add(Episode(title, duration))
+            Episode(title, duration)
         }
-    }
-    return episodes
+        .toList() // Convert Sequence back to List
 }
+
+// Extension function to convert NodeList to Sequence
+private fun NodeList.asSequence(): Sequence<Node> =
+    (0 until length).asSequence().map { item(it) }
 
 private fun selectEpisodes(episodes: List<Episode>, tempo: Int): List<String> {
     val shuffledEpisodes = episodes.shuffled()
