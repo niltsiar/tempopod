16fdc4af05c7184a3f4e25708759c628e2e0d757
Borja Quevedo <borjota@gmail.com>
2024-03-21 18:46:54 +0100

    Handle empty feed

--- app/src/jvmMain/kotlin/dev/niltsiar/tempopod/main.kt
+++ app/src/jvmMain/kotlin/dev/niltsiar/tempopod/main.kt
@@ -11,6 +11,8 @@ import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 import org.xml.sax.InputSource
 
+//Empty feed: https://raw.githubusercontent.com/webreactiva-devs/reto-tempopod/main/feed/empty.xml
+
 fun main(args: Array<String>) {
     val (selectedTempo, feedUrl) = when {
         args.size > 1 -> args[0].toInt() to args[1]
@@ -23,10 +25,14 @@ fun main(args: Array<String>) {
     if (xmlContentResult.isSuccess) {
         val xmlContent = xmlContentResult.getOrNull()
         val episodes = parseEpisodes(xmlContent!!)
-        val selectedEpisodes = episodes.selectEpisodes(selectedTempo)
+        if (episodes.none()) {
+            println("No episodes found in the feed.")
+        } else {
+            val selectedEpisodes = episodes.selectEpisodes(selectedTempo)
 
-        println("Selected episodes:")
-        selectedEpisodes.forEach { println(it) }
+            println("Selected episodes:")
+            selectedEpisodes.forEach { println(it) }
+        }
     } else {
         val exception = xmlContentResult.exceptionOrNull()
         println("Error fetching feed: ${exception?.message}")
@@ -38,15 +44,19 @@ private fun parseEpisodes(xmlContent: String): Sequence<Episode> {
         .newDocumentBuilder()
         .parse(InputSource(StringReader(xmlContent)))
 
-    return document.getElementsByTagName("item")
-        .asSequence() // Convert NodeList to Sequence for more idiomatic Kotlin
-        .filterIsInstance<Element>() // Filter out non-Element nodes
-        .map { element ->
-            val title = element.getElementsByTagName("title").item(0).textContent
-            val durationStr = element.getElementsByTagName("itunes:duration").item(0).textContent
-            val duration = durationStr.toInt()
-            Episode(title, duration)
-        }
+    val nodeList = document.getElementsByTagName("item")
+    return if (nodeList.length == 0) {
+        emptySequence()
+    } else {
+        nodeList.asSequence()
+            .filterIsInstance<Element>()
+            .map { element ->
+                val title = element.getElementsByTagName("title").item(0).textContent
+                val durationStr = element.getElementsByTagName("itunes:duration").item(0).textContent
+                val duration = durationStr.toInt()
+                Episode(title, duration)
+            }
+    }
 }
 
 private fun Sequence<Episode>.selectEpisodes(tempo: Int): Sequence<String> {
